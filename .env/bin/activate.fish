9c2106dcf8f0fc704a60daa795756d0a97108655
Rodrigue BATIEBO <rodriguebatiebo5@gmail.com>
2026-02-10 18:53:19 +0000

    resolution de probleme lier a l'environnement virtuel

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/b_rodrigue/Bureau/projet_web_blog/.env
+set -gx VIRTUAL_ENV /home/b_rodrigue/Bureau/projet_blog/.env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
